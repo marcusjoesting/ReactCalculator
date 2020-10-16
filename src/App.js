4a59ffef4ea29e6ac7b5338fb29733915de880d8
marcusjoesting <mjoesting@ugpinc.com>
2020-10-15 22:49:44 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Grid from '@material-ui/core/Grid'
+import {makeStyles} from '@material-ui/core/styles'
+import {NumberButton, SpecialButton, OperatorButton} from './Buttons'
+import Output from './Output'
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent:'center',
+    alignItems: 'center',
+    background: 'linear-gradient(to left, blue, purple)',
+    height: '100vh'
+  },
+  container: {
+    width: 400,
+    border: '3px solid black'
+  }
+}))
+
+
 
 function App() {
+  const classes = useStyles()
+  const [currentNum, setCurrent] = useState('')
+  const [prevNum, setPrev] = useState('')
+  const [operator, setOperator] = useState(undefined)
+
+
+  const clear = () => {
+    setCurrent('')
+    setPrev('')
+    setOperator(undefined)
+  }
+
+  const addNumber = number => {
+    setCurrent(x => {
+     return x.toString() + number.toString()
+    })
+  }
+
+  const deleteNum = () => {
+    setCurrent(x => {
+      return x.toString().slice(0,-1)
+    })
+  }
+
+  const chooseOperation = op => {
+    if (currentNum === '') return
+    if (prevNum !== '') calculate()
+    setOperator(op)
+    setPrev(currentNum)
+    setCurrent('')
+  }
+
+  const calculate = () => {
+    let computation
+    const prev = parseFloat(prevNum)
+    const current = parseFloat(currentNum)
+    if (isNaN(prev) || isNaN(current)) return
+    switch (operator) {
+      case '+':
+        computation = prev + current
+        break
+      case '-':
+        computation = prev - current
+        break
+      case '*':
+        computation = prev * current
+        break
+      case '÷':
+        computation = prev / current
+        break
+      case 'sqrt':
+        computation = Math.sqrt(current)
+        break
+      default:
+        return
+    }
+    setCurrent(computation)
+    setPrev('')
+    setOperator(undefined)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" className={classes.root}>
+        <Grid container justify='center' className={classes.container}>
+          <Output currentNum={currentNum} prevNum={prevNum} operator={operator}/>
+          <SpecialButton size={2} onClick={clear} text='ac'/>
+          <SpecialButton  onClick={deleteNum} text='del'/>
+          {/* <OperatorButton operator='sqrt' chooseOperation={chooseOperation}/> */}
+          <OperatorButton operator='÷' chooseOperation={chooseOperation}/>
+          <NumberButton addNumber={addNumber} number={1}/>
+          <NumberButton addNumber={addNumber} number={2}/>
+          <NumberButton addNumber={addNumber} number={3}/>
+          <OperatorButton operator='*' chooseOperation={chooseOperation}/>
+          <NumberButton addNumber={addNumber} number={4}/>
+          <NumberButton addNumber={addNumber} number={5}/>
+          <NumberButton addNumber={addNumber} number={6}/>
+          <OperatorButton operator='+' chooseOperation={chooseOperation}/>
+          <NumberButton addNumber={addNumber} number={7}/>
+          <NumberButton addNumber={addNumber} number={8}/>
+          <NumberButton addNumber={addNumber} number={9}/>
+          <OperatorButton operator='-' chooseOperation={chooseOperation}/>
+          <NumberButton addNumber={addNumber} number={'.'}/>
+          <NumberButton addNumber={addNumber} number={0}/>
+          <SpecialButton size={2} onClick={calculate} text='='/>
+        </Grid>
     </div>
   );
 }
